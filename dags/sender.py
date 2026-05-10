3b5a03b359309e382827bb6f15826bc2e0104587
Julien Papillon <julien.papillon@gmail.com>
2026-05-10 16:00:24 +0200

    feat: Create a random continuous DAG able to send events

--- dags/sender.py
+++ dags/sender.py
@@ -3,17 +3,22 @@ from pendulum import datetime
 
 @dag(
     start_date=datetime(2025, 1, 1),
-    schedule="@daily",
-    catchup=True
+    schedule="@continuous",
+    max_active_runs=1
 )
-def schedule():
+def sender():
 
     @task
     def my_task(data_interval_start: datetime = None, data_interval_end: datetime = None, logical_date: datetime = None):
+        import time
+        import random
+
+        sleep_time = random.randint(1, 10)
+        time.sleep(sleep_time)
         print(f"data_interval_start: {data_interval_start}")
         print(f"data_interval_end: {data_interval_end}")
         print(f"logical_date: {logical_date}")
     
     my_task()
 
-schedule()
+sender()
